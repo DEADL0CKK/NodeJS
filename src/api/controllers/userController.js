cdc93aac7c235500fa31b84996b4e37dca69482a
DEADL0CK <Hydrark07@gmail.com>
2020-11-24 18:16:02 +0100

    EMAIL REGEX

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,38 +1,47 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
+let verifEmail = new RegExp('^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$');
+
 
 exports.create_an_user = (req, res) => {
-    bcrypt.hash(req.body.password, 10, (error, hash) => {
-        if (error) {
-            res.status(500);
-            console.log(error);
-            res.json({
-                message: "Erreur serveur."
-            })
-        } else {
-            let new_user = new User({
-                ...req.body,
-                password: hash
-            });
-            console.log(new_user)
-            new_user.save((error, user) => {
-                if (error) {
-                    res.status(500);
-                    console.log(error);
-                    res.json({
-                        message: "Erreur serveur."
-                    })
-                } else {
-                    res.status(201);
-                    res.json({
-                        message: `Utilisateur crée :  ${user.email}`
-                    })
-                }
-            })
-        }
-    })
+    if (!verifEmail.test(req.body.email)) {
+        res.status(500);
+        res.json({
+            message: "Email ou mot de passe incorrecte ."
+        })
 
+    } else {
+        bcrypt.hash(req.body.password, 10, (error, hash) => {
+            if (error) {
+                res.status(500);
+                console.log(error);
+                res.json({
+                    message: "Erreur serveur."
+                })
+            } else {
+                let new_user = new User({
+                    ...req.body,
+                    password: hash
+                });
+                console.log(new_user)
+                new_user.save((error, user) => {
+                    if (error) {
+                        res.status(500);
+                        console.log(error);
+                        res.json({
+                            message: "Erreur serveur."
+                        })
+                    } else {
+                        res.status(201);
+                        res.json({
+                            message: `Utilisateur crée :  ${user.email}`
+                        })
+                    }
+                })
+            }
+        })
+    }
 
 
 
@@ -40,6 +49,8 @@ exports.create_an_user = (req, res) => {
 }
 
 exports.login_an_user = (req, res) => {
+
+
     User.findOne({
         email: req.body.email
     }, (error, user) => {
@@ -50,41 +61,51 @@ exports.login_an_user = (req, res) => {
                 message: "Erreur serveur."
             })
         } else {
-            bcrypt.compare(req.body.password, user.password, (error, result) => {
-                if (error) {
-                    res.status(500);
-                    console.log(error);
-                    res.json({
-                        message: "Erreur serveur."
-                    })
-                } else {
-                    jwt.sign({
-                            email: user.email,
-                            role: "user"
-                        },
-                        process.env.JWT_TOKEN, {
-                            expiresIn: '30 days'
-                        }, (error, token) => {
-                            if (error) {
-                                res.status(400);
-                                console.log("ERREURR", error);
-                                res.json({
-                                    message: "Mot de passe ou email erroné."
-                                })
-                            } else {
-                                console.log({
-                                    token
-                                })
-                                res.json({
-                                    token
-                                })
-                            }
-                        })
+            if (!verifEmail.test(req.body.email) || user === null) {
+                res.status(500);
+                console.log('Email / Mot de passe incorrecte')
+                res.json({
+                    message: "Email ou mot de passe incorrecte ."
+                })
 
-                }
-            });
+            } else {
+                bcrypt.compare(req.body.password, user.password, (error, result) => {
+                    if (error) {
+                        res.status(500);
+                        console.log(error);
+                        res.json({
+                            message: "Erreur serveur."
+                        })
+                    } else {
+                        jwt.sign({
+                                email: user.email,
+                                role: "user"
+                            },
+                            process.env.JWT_TOKEN, {
+                                expiresIn: '30 days'
+                            }, (error, token) => {
+                                if (error) {
+                                    res.status(400);
+                                    console.log("ERREURR", error);
+                                    res.json({
+                                        message: "Mot de passe ou email erroné."
+                                    })
+                                } else {
+                                    console.log({
+                                        token
+                                    })
+                                    res.json({
+                                        token
+                                    })
+                                }
+                            })
+
+                    }
+                });
+            }
 
         }
 
     })
+
 }
